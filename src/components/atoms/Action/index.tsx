e55081afac497fb745844e5d026557ba99b41400
Stackbit Code Editor <projects@stackbit.com>
2022-05-23 19:38:21 +0000

    index.tsx: changed

--- src/components/atoms/Action/index.tsx
+++ src/components/atoms/Action/index.tsx
@@ -34,7 +34,34 @@ export default function Action(props) {
         : {};
 
     const getLink = () => (
-        
+        <Link
+            href={url}
+            aria-label={altText}
+            id={elementId || null}
+            className={classNames(
+                'sb-component',
+                'sb-component-block',
+                type === 'Button' ? 'sb-component-button' : 'sb-component-link',
+                {
+                    'sb-component-button-primary': type === 'Button' && style === 'primary',
+                    'sb-component-button-secondary': type === 'Button' && style === 'secondary',
+                    'sb-component-button-icon': type === 'Button' && !label
+                },
+                className
+            )}
+            {...annotations}
+        >
+            {label && <span>{label}</span>}
+            {showIcon && IconComponent && (
+                <IconComponent
+                    className={classNames('fill-current h-5 w-5', {
+                        'order-first': iconPosition === 'left',
+                        'mr-1.5': label && iconPosition === 'left',
+                        'ml-1.5': label && iconPosition === 'right'
+                    })}
+                />
+            )}
+        </Link>
     )
 
     return (
