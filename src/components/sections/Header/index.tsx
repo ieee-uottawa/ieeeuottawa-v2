1872ab7886403897b6e52daab573d5be1fc91abb
Stackbit Code Editor <projects@stackbit.com>
2022-05-24 16:48:25 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -190,7 +190,7 @@ function SiteLogoLink({ title, isTitleVisible, logo }) {
 function ListOfLinks({ links, inMobileMenu }) {
     return links.map((link, index) => (
         <li key={index}>
-            {link.type === "" ? 
+            { link.type === "" ? 
                 <Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
             }
         </li>
