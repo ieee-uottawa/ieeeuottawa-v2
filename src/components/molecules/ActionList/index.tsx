3eadafaa5f61f0bf47b7d0be13540ecc93695a22
Stackbit Code Editor <projects@stackbit.com>
2022-05-24 16:52:30 +0000

    index.tsx: changed

--- src/components/molecules/ActionList/index.tsx
+++ src/components/molecules/ActionList/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import classNames from 'classnames';
-import Link from '../Link';
+import Link from '../../atoms/Link';
 import { iconMap } from '../../svgs';
 
 export default function ActionList(props) {
