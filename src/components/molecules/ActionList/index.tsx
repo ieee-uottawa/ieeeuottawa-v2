beadfad34bd451b81e80f18a889d1f28e106e719
Stackbit Code Editor <projects@stackbit.com>
2022-05-24 16:54:37 +0000

    index.tsx: changed

--- src/components/molecules/ActionList/index.tsx
+++ src/components/molecules/ActionList/index.tsx
@@ -7,47 +7,5 @@ export default function ActionList(props) {
         links
     } = props;
     
-    const getLink = () => (
-        <Link
-            href={url}
-            aria-label={altText}
-            id={elementId || null}
-            className={classNames(
-                'sb-component',
-                'sb-component-block',
-                type === 'Button' ? 'sb-component-button' : 'sb-component-link',
-                {
-                    'sb-component-button-primary': type === 'Button' && style === 'primary',
-                    'sb-component-button-secondary': type === 'Button' && style === 'secondary',
-                    'sb-component-button-icon': type === 'Button' && !label
-                },
-                className
-            )}
-            {...annotations}
-        >
-            {label && <span>{label}</span>}
-            {showIcon && IconComponent && (
-                <IconComponent
-                    className={classNames('fill-current h-5 w-5', {
-                        'order-first': iconPosition === 'left',
-                        'mr-1.5': label && iconPosition === 'left',
-                        'ml-1.5': label && iconPosition === 'right'
-                    })}
-                />
-            )}
-        </Link>
-    )
-
-/*  
-    if (type === "LinkList") {
-        {links} = props;
-        return (
-            <div>
-                get
-            </div>
-        )
-    }
-*/
-
-    return getLink();
+        return getLink();
 }
