903f9e89bbc401acceed026bc085c01e0c819c16
Stackbit Code Editor <projects@stackbit.com>
2022-05-24 17:57:26 +0000

    index.tsx: changed

--- src/components/molecules/ActionList/index.tsx
+++ src/components/molecules/ActionList/index.tsx
@@ -7,7 +7,7 @@ export default function ActionList(props) {
     const { label, links } = linkListObject;
     return (
         <div className="hidden group-hover:block">
-            <button class="dropbtn">{label}
+            <button className="dropbtn">{label}
             <i class="fa fa-caret-down"></i>
             </button>
             {links.map((link, index) => (
