db7de42ff696d69212f15c84c19342980c7c357f
Stackbit Code Editor <projects@stackbit.com>
2022-05-24 17:55:26 +0000

    index.tsx: changed

--- src/components/molecules/ActionList/index.tsx
+++ src/components/molecules/ActionList/index.tsx
@@ -7,6 +7,7 @@ export default function ActionList(props) {
 
     return (
         <div className="hidden group-hover:block">
+            
             {links.map((link, index) => (
                 <li key={index}>
                     <Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
