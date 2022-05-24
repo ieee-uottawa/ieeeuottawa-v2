ac6364efddc5f85b4fb145672fe275ff789ba9eb
Stackbit Code Editor <projects@stackbit.com>
2022-05-24 17:35:37 +0000

    index.tsx: changed

--- src/components/molecules/ActionList/index.tsx
+++ src/components/molecules/ActionList/index.tsx
@@ -5,9 +5,13 @@ import Action from '../../atoms/Action';
 export default function ActionList(props) {
     const { links, inMobileMenu } = props;
 
-    return links.map((link, index) => (
+    return (
+        <div>
+            {links.map((link, index) => (
         <li key={index}>
             <Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
         </li>
-    ));
+    ));}
+        </div>
+    )
 }
