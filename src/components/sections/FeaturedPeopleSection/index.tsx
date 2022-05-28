201e1889d34cb72a62c56717be52c4e617984558
Stackbit Code Editor <projects@stackbit.com>
2022-05-28 14:30:13 +0000

    index.tsx: changed

--- src/components/sections/FeaturedPeopleSection/index.tsx
+++ src/components/sections/FeaturedPeopleSection/index.tsx
@@ -143,11 +143,11 @@ function PeopleVariantB(props) {
 
                     <CardContent>
                         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
+                    This impressive paella is a perfect party dish and a fun meal to cook
+                    together with your guests. Add 1 cup of frozen peas along with the mussels,
+                    if you like.
+                    </Typography>
+                </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
