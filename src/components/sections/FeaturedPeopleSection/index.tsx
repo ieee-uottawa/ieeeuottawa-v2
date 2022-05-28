194e6cbbe783bb1bd971bca3694bc07964e3ab5d
Stackbit Code Editor <projects@stackbit.com>
2022-05-28 14:29:39 +0000

    index.tsx: changed

--- src/components/sections/FeaturedPeopleSection/index.tsx
+++ src/components/sections/FeaturedPeopleSection/index.tsx
@@ -134,20 +134,7 @@ function PeopleVariantB(props) {
             ))}
             {/**
                 <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
+
       <CardMedia
         component="img"
         height="194"
