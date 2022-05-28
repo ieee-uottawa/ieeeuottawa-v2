bb0a6e3196f47edcc0bb5d90905bb4d3142d9363
Stackbit Code Editor <projects@stackbit.com>
2022-05-28 15:09:36 +0000

    index.tsx: changed

--- src/components/sections/FeaturedPeopleSection/index.tsx
+++ src/components/sections/FeaturedPeopleSection/index.tsx
@@ -95,7 +95,7 @@ function PeopleVariantA(props) {
                             {person.bio}
                         </Markdown>*/}
                         <Social type="social" altText="Mail" label="Mail" url={person.bio} icon="mail"
-                         *          {...link} data-sb-field-path={`.${index}`}
+                         {...link} data-sb-field-path={`.${index}`}
                          />
                         /**
                          * <p>
