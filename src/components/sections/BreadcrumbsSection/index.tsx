8b69e97f88dd3bce01f27e61c8cd94aaca06ec51
Stackbit Code Editor <projects@stackbit.com>
2022-05-29 15:43:11 +0000

    index.tsx: changed

--- src/components/sections/BreadcrumbsSection/index.tsx
+++ src/components/sections/BreadcrumbsSection/index.tsx
@@ -22,7 +22,7 @@ export default function BreadcrumbsSection(props) {
                 // 'w-full',
                 // 'flex',
                 mapStyles({ justifyContent: styles.self?.justifyContent ?? 'left' }),
-                mapMaxWidthStyles({ width})
+                mapMaxWidthStyles({ widthL})
                 styles.self?.padding ?? 'py-12 px-4'
             )}
 
