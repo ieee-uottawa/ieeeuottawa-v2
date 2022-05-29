d6cfc7456d7545409e2062015e8798554a2ee8e1
Stackbit Code Editor <projects@stackbit.com>
2022-05-29 15:13:58 +0000

    index.tsx: changed

--- src/components/sections/BreadcrumbsSection/index.tsx
+++ src/components/sections/BreadcrumbsSection/index.tsx
@@ -26,7 +26,7 @@ export default function BreadcrumbsSection(props) {
         >
             <Breadcrumbs aria-label="breadcrumb">
                 {navLinks.map((link, index) => 
-                    
+                                    
                 )}
             </Breadcrumbs>
         </div >
