833ae139f28420de53bf5a8f9cf40b7b5764131d
Stackbit Code Editor <projects@stackbit.com>
2022-05-29 11:20:47 +0000

    index.tsx: changed

--- src/components/sections/FeaturedPeopleSection/index.tsx
+++ src/components/sections/FeaturedPeopleSection/index.tsx
@@ -76,9 +76,9 @@ function PeopleVariantA(props) {
                 <Card sx={{ maxWidth: 345 }}>
                     <div className="flex justify-start pt-4"
                     >
-                            <div className="h-0 w-full pt-1/1 relative mb-4">
-                                <ImageBlock {...person.image} className="absolute left-0 h-full object-cover top-0 w-full w-300 h-300 rounded-full" data-sb-field-path=".image" />
-                            </div>
+                        <div className="h-0 w-full pt-1/1 relative mb-4">
+                            <ImageBlock {...person.image} className="absolute left-0 h-full object-cover top-0 w-full w-300 h-300 rounded-full" data-sb-field-path=".image" />
+                        </div>
                     </div>
                     <CardContent>
                         {(person.firstName || person.lastName) && (
