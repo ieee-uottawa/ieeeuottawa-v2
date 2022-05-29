e1f7188326fe9a5309d73d748e2e9955592e5f0b
Stackbit Code Editor <projects@stackbit.com>
2022-05-29 15:21:29 +0000

    index.tsx: changed

--- src/components/sections/BreadcrumbsSection/index.tsx
+++ src/components/sections/BreadcrumbsSection/index.tsx
@@ -24,7 +24,7 @@ export default function BreadcrumbsSection(props) {
                 styles.self?.padding ?? 'py-12 px-4'
             )}
         >
-            <Breadcrumbs aria-label="breadcrumb" className="">
+            <Breadcrumbs aria-label="breadcrumb" className="text-left">
                 {navLinks.map((link, index) =>
                     <Action {...link} data-sb-field-path={`.${index}`} />
                 )}
