0782b73b5c6c80edb1c4b44b36fe2dd22d6f2317
Stackbit Code Editor <projects@stackbit.com>
2022-05-29 15:07:25 +0000

    index.tsx: changed

--- src/components/sections/BreadcrumbsSection/index.tsx
+++ src/components/sections/BreadcrumbsSection/index.tsx
@@ -25,19 +25,19 @@ export default function BreadcrumbsSection(props) {
         >
             <div
                 <Breadcrumbs aria-label="breadcrumb">
-  <Link underline="hover" color="inherit" href="/">
-    MUI
+                <Link underline="hover" color="inherit" href="/">
+                    MUI
   </Link>
-  <Link
-    underline="hover"
-    color="inherit"
-    href="/material-ui/getting-started/installation/"
-  >
-    Core
+                <Link
+                    underline="hover"
+                    color="inherit"
+                    href="/material-ui/getting-started/installation/"
+                >
+                    Core
   </Link>
-  <Typography color="text.primary">Breadcrumbs</Typography>
+                <Typography color="text.primary">Breadcrumbs</Typography>
 </Breadcrumbs>
             ></div>
-        </div>
+        </div >
     );
 }
