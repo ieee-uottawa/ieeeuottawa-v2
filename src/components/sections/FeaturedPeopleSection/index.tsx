ac94eb356bfafdd3d1252e53660ddae3ac8ea426
Stackbit Code Editor <projects@stackbit.com>
2022-05-29 10:58:23 +0000

    index.tsx: changed

--- src/components/sections/FeaturedPeopleSection/index.tsx
+++ src/components/sections/FeaturedPeopleSection/index.tsx
@@ -70,6 +70,7 @@ function PeopleVariantA(props) {
     return (
         <div className={classNames('grid', 'gap-x-8', 'gap-y-10', 'sm:grid-cols-2', 'lg:grid-cols-4', { 'mt-12': hasTopMargin })} data-sb-field-path=".people">
             {people.map((person, index) => (
+                
                 /* <article key={index} data-sb-field-path={`.${index}`}>
                     {person.image && (
                         <div className="h-0 w-full pt-1/1 relative mb-4">
