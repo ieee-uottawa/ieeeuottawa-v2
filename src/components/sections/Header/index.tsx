abbcf552b99556b3d093e6a61aa4b0ce0b5bce27
Stackbit Code Editor <projects@stackbit.com>
2022-07-28 15:08:30 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -161,7 +161,17 @@ function MobileMenu(props) {
                     {secondaryLinks.length > 0 && (
                         <ul className="mb-10 space-y-5" data-sb-field-path=".secondaryLinks">
                             <ListOfLinks links={secondaryLinks} inMobileMenu={true} />
-                            
+                            <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    value={age}
+    label="Age"
+    onChange={handleChange}
+  >
+    <MenuItem value={10}>Ten</MenuItem>
+    <MenuItem value={20}>Twenty</MenuItem>
+    <MenuItem value={30}>Thirty</MenuItem>
+  </Select>
                         </ul>
                     )}
                 </div>
