284869433c0af8f4a7bcd7cac42794ee0e520c3b
Stackbit Code Editor <projects@stackbit.com>
2022-07-28 15:09:30 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -7,6 +7,7 @@ import { Link, Action } from '../../atoms';
 import ImageBlock from '../../molecules/ImageBlock';
 import CloseIcon from '../../svgs/close';
 import MenuIcon from '../../svgs/menu';
+import { Select, MenuItem } from '@mui/material';
 
 export default function Header(props) {
     const { isSticky, primaryColors = 'colors-d', styles = {}, annotationPrefix, ...rest } = props;
