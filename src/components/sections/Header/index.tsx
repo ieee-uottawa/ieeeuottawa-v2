3f78db0ca24aa03ff1a0bfaf5e1b8a5f38253595
Stackbit Code Editor <projects@stackbit.com>
2022-07-28 15:08:56 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -167,11 +167,11 @@ function MobileMenu(props) {
                                 value={age}
                                 label="Age"
                                 onChange={handleChange}
-  >
-    <MenuItem value={10}>Ten</MenuItem>
-    <MenuItem value={20}>Twenty</MenuItem>
-    <MenuItem value={30}>Thirty</MenuItem>
-  </Select>
+                            >
+                                <MenuItem value={10}>Ten</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
                         </ul>
                     )}
                 </div>
