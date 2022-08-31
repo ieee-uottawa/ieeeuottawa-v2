9369c3f66fbb283d9c4d0a7bef62730203426e46
Stackbit Code Editor <projects@stackbit.com>
2022-08-31 23:26:29 +0000

    index.tsx: changed

--- src/components/molecules/ImageBlock/index.tsx
+++ src/components/molecules/ImageBlock/index.tsx
@@ -12,8 +12,8 @@ export default function ImageBlock(props) {
         : {};
 
     const myLoader = ({ src, width, quality }) => {
-  return `https://example.com/${src}?w=${width}&q=${quality || 75}`
-}
+        return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+        }
     
     const getImage = () => (
         <div className={classNames('sb-component', 'sb-component-block', 'sb-component-image-block', className)}>
