8f216a2103126395c3256912715c2ef27b87acb5
Stackbit Code Editor <projects@stackbit.com>
2022-08-31 23:28:48 +0000

    index.tsx: changed

--- src/components/molecules/ImageBlock/index.tsx
+++ src/components/molecules/ImageBlock/index.tsx
@@ -14,7 +14,7 @@ export default function ImageBlock(props) {
     const base_url = process.env.base_url || 'https://best-robin-49c95.stackbit.app/';
 
     const loader = ({ src, width, quality }) => {
-        return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+        return `${base_url}${src}?w=${width}&q=${quality || 75}`
     }
     
     const getImage = () => (
